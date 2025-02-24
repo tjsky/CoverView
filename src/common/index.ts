c41e90381b1abf8ac1f59d8117e297271e4b73c4
去年夏天 <7272911+tjsky@users.noreply.github.com>
2025-02-24 09:49:18 +0800

    自用便利性修改

--- src/common/index.ts
+++ src/common/index.ts
@@ -9,20 +9,20 @@ import theme6 from '@/assets/images/theme6.webp'
 import theme7 from '@/assets/images/theme7.webp'
 
 export const defaultSettings: ThemeConfig = {
-  title: 'A begineers guide to frontend development',
-  bgColor: '#949ee5',
-  pattern: '',
-  author: import.meta.env.REACT_APP_AUTHOR || 'Lruihao',
+  title: '秋风于渭水',
+  bgColor: '#5bc0eb',
+  pattern: 'none',
+  author: import.meta.env.REACT_APP_AUTHOR || '去年夏天',
   icon: { label: 'reactjs', value: 'react', opts: ['original', 'original-wordmark'] },
   iconStyle: 'original',
   iconStyleOptions: ['original', 'original-wordmark'],
-  font: 'font-Virgil',
-  theme: 'background',
+  font: 'font-sans',
+  theme: 'stylish',
   customIcon: '',
-  platform: 'hashnode',
-  customPlatformWidth: 1024,
-  customPlatformX: 0,
-  customPlatformY: 0,
+  platform: 'dev',
+  customPlatformWidth: 1200,
+  customPlatformX: 1200,
+  customPlatformY: 630,
 }
 
 export const fontOptions: ThemeFont[] = [
